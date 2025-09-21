c1a45eb0807f0fa056bf319b974b606692113718
shubham22062 <shubhamawasthi021@gmail.com>
2025-09-21 23:00:43 +0530

    second

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) = {
+        Pormise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
